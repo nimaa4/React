71dc77f75be8f65d2d16f4d3ce21e43b506f2d92
nimaa4 <147697959+nimaa4@users.noreply.github.com>
2023-10-22 13:29:17 +0530

    Add files via upload

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+class Footer extends Component {
+render() {
+    return (
+        <>
+        <hr />
+        <h3>contact</h3>
+        <p>
+            <b>Mobile:</b>123456789
+        </p>
+        <p>
+            <b>Email</b>abc@gmail.com
+        </p>
+        </>
+    );
+}
+};
+export default Footer;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Header.css";
+function Header(){
+    return(
+        <div id="box">
+
+        <h1>Mahatma Gandhi</h1>
+
+        <img id="img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJGIstaoeamueRUnbaxjgxYgLtoPLznC6z3w&usqp=CAU" 
+        alt='loading...'></img>
+        </div>
+    )
+}
+export default Header;
